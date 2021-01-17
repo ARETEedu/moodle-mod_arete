fe29e25e8f9b802fd4e27c70057c48a52aeb3df4
aradjafari <arad.jafari@gmail.com>
2021-01-17 20:39:39 +0100

    First Push
    The first directory structure and db installation.

---
+++ lang/en/arete.php
@@ -0,0 +1,3 @@
+<?php
+
+$string['modulename'] = 'Arete Plugin';
---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2020017000; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2015111000; // Requires this Moodle version
+$plugin->component = 'mod_arete'; // Full name of the plugin (used for diagnostics)
