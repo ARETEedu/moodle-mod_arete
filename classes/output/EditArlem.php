baf7522d50f4938f32695b18789116d5b0b5e53e
aradjafari <arad.jafari@gmail.com>
2021-03-25 13:37:30 +0100

    bug fix

--- classes/output/EditArlem.php
+++ classes/output/EditArlem.php
@@ -36,11 +36,8 @@ class EditArlem{
 
         
         //The user editing folder
-        $path = 'mod/arete/temp/';
-        if (!file_exists($path)) {
-            mkdir($path, 0777, true);
-        }
-        $this->userDirPath = $CFG->dirroot. '/' . $path . strval($USER->id);
+        $path = '/mod/arete/temp/';
+        $this->userDirPath = $CFG->dirroot . $path . strval($USER->id);
         
         //remove temp dir which is used on editing
           $tempDir = $this->userDirPath. '/';
