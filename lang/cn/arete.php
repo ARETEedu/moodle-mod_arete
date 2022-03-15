4cad2b9c6f6783dfe9ced29985a90081fd50e58a
Fridolin Wild <fridolin.wild@gmail.com>
2022-03-15 11:27:23 +0000

    Create arete.php
    Added Chinese Language support - Thanks, Na Li from UCD :)

---
+++ lang/cn/arete.php
@@ -0,0 +1,135 @@
+<?php
+
+// This file is part of the Augmented Reality Experience plugin (mod_arete) for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    mod_arete
+ * @copyright  2021, Abbas Jafari & Fridolin Wild, Open University, UK
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @translation to Chinese: Na Li, University College Dublin, Ireland
+ */
+
+$string['modulename'] = '增强现实体验';
+$string['pluginname'] = '增强现实体验';
+$string['arname'] = '增强现实体验名字';
+$string['modname'] = 'arete';
+$string['description'] = '描述';
+$string['arlemsectiontitle'] = 'ARLEMs';
+$string['arlemradiobuttonlabel'] = '此活动中可用的Arlems';
+$string['pluginadministration'] = '增强现实体验管理';
+$string['component'] = 'mod_arete';
+$string['filearea'] = 'arlems';
+$string['datetitle'] = '创建时间';
+$string['modifieddatetitle'] = '时间修改';
+$string['neveredited'] = '绝不';
+$string['arlemtitle'] = '活动名称';
+$string['arlemthumbnail'] = '缩略图';
+$string['sizetitle'] = '尺寸';
+$string['deletetitle'] = '删除';
+$string['qrtitle'] = '二维码';
+$string['qrbutton'] = '生成';
+$string['authortitle'] = '作者';
+$string['playtitle'] = '启动';
+$string['playbutton'] = '播放';
+$string['downloadtitle'] = '下载';
+$string['downloadbutton'] = '下载';
+$string['editbutton'] = '编辑';
+$string['publictitle'] = '公共';
+$string['assigntitle'] = '分配';
+$string['ratingtitle'] = '评分';
+$string['viewstitle'] = '浏览';
+$string['assignedbytitle'] = '分配者';
+$string['notsetyet'] = '尚未设置';
+$string['assignedarlem'] = '此增强现实体验已分配给此活动';
+$string['availabledarlem'] = '可用的增强现实体验列表';
+$string['rolelabel'] = '您在此活动的角色: ';
+$string['roleundefined'] = '您的角色没有定义!';
+$string['deletenotallow'] = '-';
+$string['notassignedyer'] = '还没有分配增强现实体验! 请您的老师为这项活动添加经验.';
+$string['editpagetitle'] = '编辑 ARLEM';
+$string['accessnotallow'] = '您没有访问此页面的权限.';
+$string['accessdenied'] = '拒绝访问';
+$string['editpagetitle'] = '编辑 ARLEM 文件';
+$string['selectfiles'] = '选择需要更新的文件。您可以选择多个文件';
+$string['addthumbnail'] = '您可以通过上传 "thumbnail.jpg" 文件来为该活动添加缩略图.';
+$string['selectfileshelp'] = '现有文件将被选中的同名文件替换';
+$string['arlemstructure'] = '结构';
+$string['filenotselectedalert'] = '请至少选择一个文件来更新此活动';
+$string['choosefilesbutton'] = '选择文件';
+$string['nofileselected'] = ' 没有选择文件';
+$string['cancelbutton'] = '取消';
+$string['savebutton'] = '保存';
+$string['noarlemalert'] = '还没有 ARLEMs 存在. 你不能创建这个活动!';
+$string['detailviewtitle'] = '详细信息';
+$string['modulenameplural'] = 'Arete';
+$string['validator'] = 'ARLEM\'s JSON 验证器';
+$string['jsonnotfound'] = '找不到活动或工作场所文件!';
+$string['openvalidator'] = '打开编辑器';
+$string['closevalidator'] = '关闭';
+$string['search'] = '搜索';
+$string['nomatchfound'] = '糟糕! 没有匹配项 ';
+$string['noarlemfound'] = '糟糕! 没有要显示的活动!';
+$string['editmodedisabledbutton'] = '启用编辑模式';
+$string['editmodeenabledbutton'] = '禁用编辑模式';
+$string['termsofuse'] = '使用条款';
+$string['calibrationmarker'] = '校准图像';
+$string['newactivity'] = '新活动';
+$string['downloadmiragexr'] = '创作工具';
+$string['scoretitle'] = '分数';
+$string['votetitle'] = '投票';
+$string['youraractivities'] = '您的增强现实活动';
+$string['filedamage'] = '文件已损坏!';
+$string['voteregistered'] = '已注册!';
+$string['validatorsavemsg'] = '所有更改都应用于JSON 文件. 关闭此编辑器并单击“保存”按钮以使用这些新更改更新活动。你想关闭编辑器吗?';
+$string['savewarning'] = 'JSON 文件被编辑。要将这些更改应用于此活动，请单击“保存”按钮.';
+$string['validatorsave'] = '应用';
+$string['confirmchanges'] = '我声明我已经理解删除文件是不可逆的行为';
+$string['incorrectcourseid'] = '课程编号不正确';
+$string['profileimagealt'] = '个人资料图片';
+$string['playbuttonalt'] = '在设备上启动';
+$string['downloadbuttonalt'] = '下载活动';
+$string['editbuttonalt'] = '编辑活动';
+$string['qrcodebuttonalt'] = '使用二维码加载活动';
+$string['unabletogetrating'] = '无法获取评分记录!';
+$string['unabletosetrating'] = '无法设置您的评分记录!';
+$string['paginationprev'] = '上一个';
+$string['paginationnext'] = '下一个';
+$string['privacy:metadata:arete_allarlems:sessionid'] = 'Unity将生成的活动号码.';
+$string['privacy:metadata:arete_allarlems:filename'] = '压缩文件的名字保存在文件系统中.';
+$string['privacy:metadata:arete_allarlems:title'] = '文件的标题显示在两者的插件前端和MirageXR 应用程序.';
+$string['privacy:metadata:arete_allarlems:filesize'] = '活动压缩文件的大小.';
+$string['privacy:metadata:arete_allarlems:rate'] = '活动的平均评分.';
+$string['privacy:metadata:arete_allarlems:workplace_json'] = '活动的工作场所JSON 字符串.';
+$string['privacy:metadata:arete_allarlems:activity_json'] = '活动的活动JSON 字符串.';
+$string['privacy:metadata:arete_allarlems:timecreated'] = '活动创建的时间.';
+$string['privacy:metadata:arete_allarlems:timemodified'] = '活动被修改的时间.';
+$string['privacy:metadata:arete_allarlems:contextid'] = 'arete 插件上下文为 1.';
+$string['privacy:metadata:arete_allarlems:fileid'] = '文件表中存储文件号码.';
+$string['privacy:metadata:arete_allarlems:userid'] = '创建活动的用户号码.';
+$string['privacy:metadata:arete_allarlems:itemid'] = '表示活动的唯一号码.';
+$string['privacy:metadata:arete_allarlems:views'] = 'MirageXR 应用中活动的查看次数.';
+$string['privacy:metadata:arete_allarlems:upublic'] = '活动在Moodle上的可见性状态.';
+$string['privacy:metadata:arete_allarlems'] = '由MirageXR创建的所有活动的信息.';
+$string['privacy:metadata:arete_arlem:areteid'] = 'arete实例的号码.';
+$string['privacy:metadata:arete_arlem:arlemid'] = 'ARLEM活动的号码.';
+$string['privacy:metadata:arete_arlem:teacherid'] = '将活动分配给arete实例的用户号码.';
+$string['privacy:metadata:arete_arlem:timecreated'] = 'ARLEM 活动分配给arete实例的时间.';
+$string['privacy:metadata:arete_arlem'] = '分配给arete实例的ARLEM活动信息.';
+$string['privacy:metadata:arete_rating:userid'] = '已评分的用户号码.';
+$string['privacy:metadata:arete_rating:itemid'] = '已被评级的ARLEM活动的号码.';
+$string['privacy:metadata:arete_rating:rating'] = '用户给出的ARLEM活动的评分.';
+$string['privacy:metadata:arete_rating:timecreated'] = '提交评分的时间.';
+$string['privacy:metadata:arete_rating'] = 'ARLEM活动评级信息.';
