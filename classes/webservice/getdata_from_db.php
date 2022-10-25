5fb4cd2bb239f37c71029c9cb73621773678a868
Callustian <Callustian@users.noreply.github.com>
2022-10-25 13:30:28 +0100

    (98) Deleting arlem from other tables

--- classes/webservice/getdata_from_db.php
+++ classes/webservice/getdata_from_db.php
@@ -290,9 +290,10 @@ function delete_arlem() {
     }
 
     $fileReference = $DB->get_field('arete_allarlems', 'filename', array('itemid' => $itemid, 'sessionid' => $sessionid));
+    $fileID = $DB->get_field('arete_allarlems', 'fileid', array('itemid' => $itemid, 'sessionid' => $sessionid));
 
-    if (isset($itemid) && $fileReference !== null) {
-        $result = delete_arlem_from_plugin($fileReference, $itemid, $sessionid);
+    if (isset($itemid) && $fileReference !== null && $fileID !== null) {
+        $result = delete_arlem_from_plugin($fileReference, $itemid, $sessionid, $fileID);
         print_r(json_encode($result));
     } else {
         //The text will be used on the webservice app, therefore it is hardcoded
@@ -307,13 +308,16 @@ function delete_arlem() {
  * @global object $DB The Moodle database object
  * int $itemid The id of the ARLEM in arete_allalrems table
  * Object $fileReference the file reference of the file we have to delete
+ * Object $fileID the fileId of the arlem
  **/
-function delete_arlem_from_plugin($fileReference, $itemid, $sessionid){
+function delete_arlem_from_plugin($fileReference, $itemid, $sessionid, $fileID){
     global $DB;
 
     if (!empty($fileReference)) {
         mod_arete_delete_arlem_from_plugin($fileReference, $itemid);
         $DB->delete_records('arete_allarlems', array('sessionid' => $sessionid, 'itemid' =>$itemid));
+        $DB->delete_records('arete_arlem', array('arlemid'=> $fileID));
+        $DB->delete_records('arete_rating', array('itemid' =>$itemid));
         return true;
     }
     return false;
